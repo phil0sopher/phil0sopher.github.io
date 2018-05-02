4e5a5033628a5f85cb625f301b3a69960b727519
phil0sopher <philipc.kuo@gmail.com>
2018-05-01 23:58:42 -0700

    5.1.2018 commit 1
    Made font-weight lighter, moved some items, added Lightbox to several
    pages, made shadows less dramatic, added design challenge, and added
    new images/redited resume

--- js/customJS.js
+++ js/customJS.js
@@ -4,3 +4,13 @@ if ( $(window).width() > 416) {
 else {
   $('iframe.disable_mobile').attr('src','null'); 
 }
+
+  var elem = document.querySelector('.materialboxed');
+  var instance = M.Materialbox.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+        
